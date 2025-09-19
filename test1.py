e7060a8511ba326e75edb4f646cac66ede68dfbd
Premkumar25-progmmer <yadavpremkumar436@gmail.com>
2025-09-19 15:16:49 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+num1=st.number_input("enter number 1 value:")
+num2=st.number_input("enter number 2 value:")
+st.write("addition is:",num1+num2)
+st.write("subtraction is:",num1-num2)
+st.write("multipliction is:",num1*num2)
